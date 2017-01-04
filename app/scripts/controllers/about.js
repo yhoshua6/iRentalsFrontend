82a42a6cf0cb805183cd1df1939ae64e8cfbced1
José <jose.epifanio90@gmail.com>
2017-01-04 13:08:28 -0600

    initial commit.

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name iRentalsFrontApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the iRentalsFrontApp
+ */
+angular.module('iRentalsFrontApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
