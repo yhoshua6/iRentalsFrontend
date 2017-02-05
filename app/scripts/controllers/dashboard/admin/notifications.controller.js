aef0f2e15c3c2931249699de21c79329d06d0bc5
José <jose.epifanio90@gmail.com>
2017-02-05 12:43:40 -0600

    finished the skeletons of all views and started with the file uploading.

--- app/scripts/controllers/dashboard/admin/notifications.controller.js
+++ app/scripts/controllers/dashboard/admin/notifications.controller.js
@@ -7,23 +7,105 @@
     .controller("notificationsCtrl", notificationsCtrl);
 
 
-  notificationsCtrl.$inject = ["$log", "$state"];
-  function notificationsCtrl($log, $state) {
+  notificationsCtrl.$inject = ["$log", "$mdDialog", "$mdEditDialog"];
+  function notificationsCtrl($log, $mdDialog, $mdEditDialog) {
     //if (!isUserAlive) { $state.go("root.login"); }
     var notificationsScope = this;
+    notificationsScope.selected = [];
+    notificationsScope.query = {
+      order: 'title',
+      limit: 5,
+      page: 1
+    };
     notificationsScope.notifications = [
-      { title: "Something", toUser: "test", message: "lorem ipsum", image: "www.google.com" },
-      { title: "Something", toUser: "test", message: "lorem ipsum", image: "www.google.com" },
-      { title: "Something", toUser: "test", message: "lorem ipsum", image: "www.google.com" },
-      { title: "Something", toUser: "test", message: "lorem ipsum", image: "www.google.com" }
+      { id: 0, imageName: "dogs.jpg", title: "alsjkdlaskd", content: "something", for: "a" },
+      { id: 1, imageName: "cats.png", title: "alsjkdlaskd", content: "something", for: "b" },
+      { id: 2, imageName: "birds.jpg", title: "alsjkdlaskd", content: "something", for: "c" },
+      { id: 3, imageName: "moredogs.jpg", title: "alsjkdlaskd", content: "something", for: "z" },
+      { id: 4, imageName: "someName", title: "alsjkdlaskd", content: "something", for: "r" },
+      { id: 5, imageName: "someName", title: "alsjkdlaskd", content: "something", for: "y" },
+      { id: 6, imageName: "someName", title: "alsjkdlaskd", content: "something", for: "s" },
+      { id: 7, imageName: "someName", title: "alsjkdlaskd", content: "something", for: "f" },
+      { id: 8, imageName: "someName", title: "alsjkdlaskd", content: "something", for: "g" },
+      { id: 9, imageName: "someName", title: "alsjkdlaskd", content: "something", for: "o" },
+      { id: 10, imageName: "someName", title: "alsjkdlaskd", content: "something", for: "ce" },
+      { id: 11, imageName: "someName", title: "alsjkdlaskd", content: "something", for: "e" },
+      { id: 12, imageName: "someName", title: "alsjkdlaskd", content: "something", for: "v" },
+      { id: 13, imageName: "someName", title: "alsjkdlaskd", content: "something", for: "l" }
+
     ];
 
-    notificationsScope.deleteNotifications = function (propertyId) {
-      $log.log("----------------", propertyId);
+    notificationsScope.deleteNotifications = function () {
+      for(var i=0; i<notificationsScope.selected.length; i++)
+      {
+        notificationsScope.notifications.splice(notificationsScope.notifications.indexOf(notificationsScope.selected[i]), 1);
+      }
     };
 
-    notificationsScope.doNothing = function () {
+    notificationsScope.modifyField = function (event,fieldNumber, notification) {
+      event.stopPropagation();
+      var promise = $mdEditDialog.small(getDialogOptions(fieldNumber, notification));
+      promise.then(function (ctrl) {
+        var input = ctrl.getInput();
+        input.$viewChangeListeners.push(function () {
+          input.$setValidity('test', input.$modelValue !== 'test');
+        });
+      });
     };
+
+
+    notificationsScope.newNotification = function (event) {
+      $mdDialog.show({
+        //controller: "newGroupCtrl",
+        //controllerAs: "newGroupCtrl",
+        templateUrl: "../../../../views/dashboard/admin/templates/new_notification_modal.html",
+        parent: angular.element(document.body),
+        targetEvent: event,
+        clickOutsideToClose:true
+      })
+        .then(function(answer) {
+          //$scope.status = 'You said the information was "' + answer + '".';
+        }, function() {
+          //$scope.status = 'You cancelled the dialog.';
+        });
+    };
+
+    function getDialogOptions(option, notification) {
+      var dialogOption = null;
+      switch (option) {
+        case 1:
+          dialogOption = {
+            modelValue: notification.title,
+            placeholder: 'Cambia el título de la notificación.',
+            save: function (input) {
+              $log.log("Updating.....");
+              notification.title = input.$modelValue;
+              $log.log("Done.");
+            },
+            targetEvent: event,
+            validators: {
+              'md-maxlength': 40
+            }
+          };
+          break;
+        case 2:
+          dialogOption = {
+            modelValue: notification.content,
+            placeholder: 'Cambia el contenido de la notificación.',
+            save: function (input) {
+              $log.log("Updating.....");
+              notification.content = input.$modelValue;
+              $log.log("Done.");
+            },
+            targetEvent: event,
+            validators: {
+              'md-maxlength': 40
+            }
+          };
+          break;
+      }
+      return dialogOption;
+    }
 
   }
 })();
--- app/scripts/controllers/dashboard/admin/properties.controller.js
+++ app/scripts/controllers/dashboard/admin/properties.controller.js
@@ -7,48 +7,36 @@
     .controller("adminPropertiesCtrl", adminPropertiesCtrl);
 
 
-  adminPropertiesCtrl.$inject = ["$log", "$state", "$mdEditDialog"];
-  function adminPropertiesCtrl($log, $state, $mdEditDialog) {
-    //if (!isUserAlive) { $state.go("root.login"); }
+  adminPropertiesCtrl.$inject = ["$log", "$mdDialog", "$mdEditDialog"];
+  function adminPropertiesCtrl($log, $mdDialog, $mdEditDialog) {
     var propertiesScope = this;
-    propertiesScope.properties = [
-      { id: "0", type: "local", description: "lorem ipsum", active: "true", surfaceTotal: 0.32, surfaceIn: 10.2, surfaceOut: 2.10 },
-      { id: "1", type: "local", description: "lorem ipsum", active: "true", surfaceTotal: 0.32, surfaceIn: 10.2, surfaceOut: 2.10 },
-      { id: "2", type: "local", description: "lorem ipsum", active: "true", surfaceTotal: 0.32, surfaceIn: 10.2, surfaceOut: 2.10 },
-      { id: "3", type: "local", description: "lorem ipsum", active: "true", surfaceTotal: 0.32, surfaceIn: 10.2, surfaceOut: 2.10 },
-      { id: "4", type: "local", description: "lorem ipsum", active: "true", surfaceTotal: 0.32, surfaceIn: 10.2, surfaceOut: 2.10 },
-      { id: "5", type: "local", description: "lorem ipsum", active: "true", surfaceTotal: 0.32, surfaceIn: 10.2, surfaceOut: 2.10 },
-      { id: "6", type: "local", description: "lorem ipsum", active: "true", surfaceTotal: 0.32, surfaceIn: 10.2, surfaceOut: 2.10 },
-      { id: "7", type: "local", description: "lorem ipsum", active: "true", surfaceTotal: 0.32, surfaceIn: 10.2, surfaceOut: 2.10 },
-      { id: "8", type: "local", description: "lorem ipsum", active: "true", surfaceTotal: 0.32, surfaceIn: 10.2, surfaceOut: 2.10 }
-    ];
-
+    //if (!isUserAlive) { $state.go("root.login"); }
+    propertiesScope.selected = [];
     propertiesScope.query = {
-      order: 'description',
+      order: 'group_name',
       limit: 5,
       page: 1
     };
+    propertiesScope.properties = [
+      { id: 0, name: "someName", description: "alsjkdlaskd", active: true, surfaceTotal: 100, surfaceIn: 1.02, surfaceOut: 2.320 },
+      { id: 1, name: "someName", description: "alsjkdlaskd", active: true, surfaceTotal: 100, surfaceIn: 1.02, surfaceOut: 2.320 },
+      { id: 2, name: "someName", description: "alsjkdlaskd", active: true, surfaceTotal: 100, surfaceIn: 1.02, surfaceOut: 2.320 },
+      { id: 3, name: "someName", description: "alsjkdlaskd", active: true, surfaceTotal: 100, surfaceIn: 1.02, surfaceOut: 2.320 },
+      { id: 4, name: "someName", description: "alsjkdlaskd", active: true, surfaceTotal: 100, surfaceIn: 1.02, surfaceOut: 2.320 },
+      { id: 5, name: "someName", description: "alsjkdlaskd", active: true, surfaceTotal: 100, surfaceIn: 1.02, surfaceOut: 2.320 },
+      { id: 6, name: "someName", description: "alsjkdlaskd", active: true, surfaceTotal: 100, surfaceIn: 1.02, surfaceOut: 2.320 },
+      { id: 7, name: "someName", description: "alsjkdlaskd", active: true, surfaceTotal: 100, surfaceIn: 1.02, surfaceOut: 2.320 },
+      { id: 8, name: "someName", description: "alsjkdlaskd", active: true, surfaceTotal: 100, surfaceIn: 1.02, surfaceOut: 2.320 }
+    ];
 
-    propertiesScope.selected = [];
+    propertiesScope.deleteProperties = function () {
+      $log.log(propertiesScope.selected, "////");
 
-    propertiesScope.editDescription = function (event, property) {
-      // if auto selection is enabled you will want to stop the event
-      // from propagating and selecting the row
+    };
+
+    propertiesScope.modifyField = function (event,fieldNumber, user) {
       event.stopPropagation();
-      var promise = $mdEditDialog.small({
-        modelValue: property.description,
-        placeholder: 'Cambia el nombre del grupo.',
-        arialLabel: "editDialog",
-        save: function (input) {
-          $log.log("Updating.....");
-          property.description = input.$modelValue;
-          $log.log("Done.");
-        },
-        targetEvent: event,
-        validators: {
-          'md-maxlength': 40
-        }
-      });
+      var promise = $mdEditDialog.small(getDialogOptions(fieldNumber, user));
       promise.then(function (ctrl) {
         var input = ctrl.getInput();
         input.$viewChangeListeners.push(function () {
@@ -56,6 +44,108 @@
         });
       });
     };
+
+
+    propertiesScope.newProperty = function (event) {
+      $mdDialog.show({
+        //controller: "newGroupCtrl",
+        //controllerAs: "newGroupCtrl",
+        templateUrl: "../../../../views/dashboard/admin/templates/new_property_modal.html",
+        parent: angular.element(document.body),
+        targetEvent: event,
+        clickOutsideToClose:true
+      })
+        .then(function(answer) {
+          //$scope.status = 'You said the information was "' + answer + '".';
+        }, function() {
+          //$scope.status = 'You cancelled the dialog.';
+        });
+    };
+
+    function getDialogOptions(option, user) {
+      var dialogOption = null;
+      switch (option) {
+        case 1:
+          dialogOption = {
+            modelValue: user.name,
+            placeholder: 'Cambia el nombre de la propiedad.',
+            save: function (input) {
+              $log.log("Updating.....");
+              user.name = input.$modelValue;
+              $log.log("Done.");
+            },
+            targetEvent: event,
+            validators: {
+              'md-maxlength': 40
+            }
+          };
+          break;
+        case 2:
+          dialogOption = {
+            modelValue: user.description,
+            placeholder: 'Cambia la descripción de la propiedad.',
+            save: function (input) {
+              $log.log("Updating.....");
+              user.description = input.$modelValue;
+              $log.log("Done.");
+            },
+            targetEvent: event,
+            validators: {
+              'md-maxlength': 40
+            }
+          };
+          break;
+        case 3:
+          dialogOption = {
+            modelValue: user.surfaceTotal,
+            placeholder: 'Cambia el area total de la propiedad.',
+            arialLabel: "editDialog",
+            save: function (input) {
+              $log.log("Updating.....");
+              user.surfaceTotal = input.$modelValue;
+              $log.log("Done.");
+            },
+            targetEvent: event,
+            validators: {
+              'md-maxlength': 40
+            }
+          };
+          break;
+        case 4:
+          dialogOption = {
+            modelValue: user.surfaceIn,
+            placeholder: 'Cambia el area interna de la propiedad.',
+            arialLabel: "editDialog",
+            save: function (input) {
+              $log.log("Updating.....");
+              user.surfaceIn = input.$modelValue;
+              $log.log("Done.");
+            },
+            targetEvent: event,
+            validators: {
+              'md-maxlength': 40
+            }
+          };
+          break;
+        case 5:
+          dialogOption = {
+            modelValue: user.surfaceOut,
+            placeholder: 'Cambia la area interna de la propiedad.',
+            arialLabel: "editDialog",
+            save: function (input) {
+              $log.log("Updating.....");
+              user.surfaceOut = input.$modelValue;
+              $log.log("Done.");
+            },
+            targetEvent: event,
+            validators: {
+              'md-maxlength': 40
+            }
+          };
+          break;
+      }
+      return dialogOption;
+    }
 
   }
 })();
