41ed8eb13e13b730e01d82f824443a8270b0e07e
José <jose.epifanio90@gmail.com>
2017-02-05 01:13:43 -0600

    started the skeleton of properties.

--- app/scripts/controllers/dashboard/admin/users.controller.js
+++ app/scripts/controllers/dashboard/admin/users.controller.js
@@ -18,7 +18,6 @@
       limit: 5,
       page: 1
     };
-
     adminUserScope.users = [
       { id: 0, email: "testing@gmail.com", firstName: "test", lastName: "test", cellphone: "000-000-0000",
         bankName: "Alligator Inc", bankClabe: "asd-123", bankAccount: "123-das-123", cedula: "something"},
@@ -38,22 +37,14 @@
         bankName: "Alligator Inc", bankClabe: "asd-123", bankAccount: "123-das-123", cedula: "something"}
     ];
 
-    adminUserScope.userFirstName = function (event, user) {
+    adminUserScope.deleteUsers = function () {
+      $log.log(adminUserScope.selected, "////");
+
+    };
+
+    adminUserScope.modifyField = function (event,fieldNumber, user) {
       event.stopPropagation();
-      var promise = $mdEditDialog.small({
-        modelValue: user.firstName,
-        placeholder: 'Cambia el nombre del usuario.',
-        arialLabel: "editDialog",
-        save: function (input) {
-          $log.log("Updating.....");
-          user.firstName = input.$modelValue;
-          $log.log("Done.");
-        },
-        targetEvent: event,
-        validators: {
-          'md-maxlength': 40
-        }
-      });
+      var promise = $mdEditDialog.small(getDialogOptions(fieldNumber, user));
       promise.then(function (ctrl) {
         var input = ctrl.getInput();
         input.$viewChangeListeners.push(function () {
@@ -62,12 +53,6 @@
       });
     };
 
-
-
-    adminUserScope.deleteUsers = function () {
-      $log.log(adminUserScope.selected, "////");
-
-    };
 
     adminUserScope.newUser = function (event) {
       $mdDialog.show({
@@ -84,5 +69,122 @@
           //$scope.status = 'You cancelled the dialog.';
         });
     };
+
+    function getDialogOptions(option, user) {
+      var dialogOption = null;
+      switch (option) {
+        case 1:
+          dialogOption = {
+            modelValue: user.firstName,
+            placeholder: 'Cambia el nombre del usuario.',
+            save: function (input) {
+              $log.log("Updating.....");
+              user.firstName = input.$modelValue;
+              $log.log("Done.");
+            },
+            targetEvent: event,
+            validators: {
+              'md-maxlength': 40
+            }
+          };
+          break;
+        case 2:
+          dialogOption = {
+            modelValue: user.lastName,
+            placeholder: 'Cambia el apellido del usuario.',
+            save: function (input) {
+              $log.log("Updating.....");
+              user.lastName = input.$modelValue;
+              $log.log("Done.");
+            },
+            targetEvent: event,
+            validators: {
+              'md-maxlength': 40
+            }
+          };
+          break;
+        case 3:
+          dialogOption = {
+            modelValue: user.cellphone,
+            placeholder: 'Cambia el celular del usuario.',
+            arialLabel: "editDialog",
+            save: function (input) {
+              $log.log("Updating.....");
+              user.cellphone = input.$modelValue;
+              $log.log("Done.");
+            },
+            targetEvent: event,
+            validators: {
+              'md-maxlength': 40
+            }
+          };
+          break;
+        case 4:
+          dialogOption = {
+            modelValue: user.bankName,
+            placeholder: 'Cambia el nombre del banco,',
+            arialLabel: "editDialog",
+            save: function (input) {
+              $log.log("Updating.....");
+              user.bankName = input.$modelValue;
+              $log.log("Done.");
+            },
+            targetEvent: event,
+            validators: {
+              'md-maxlength': 40
+            }
+          };
+          break;
+        case 5:
+          dialogOption = {
+            modelValue: user.bankClabe,
+            placeholder: 'Cambia la clabe del banco,',
+            arialLabel: "editDialog",
+            save: function (input) {
+              $log.log("Updating.....");
+              user.bankClabe = input.$modelValue;
+              $log.log("Done.");
+            },
+            targetEvent: event,
+            validators: {
+              'md-maxlength': 40
+            }
+          };
+          break;
+        case 6:
+          dialogOption = {
+            modelValue: user.bankAccount,
+            placeholder: 'Cambia la cuenta de banco,',
+            arialLabel: "editDialog",
+            save: function (input) {
+              $log.log("Updating.....");
+              user.bankAccount = input.$modelValue;
+              $log.log("Done.");
+            },
+            targetEvent: event,
+            validators: {
+              'md-maxlength': 40
+            }
+          };
+          break;
+        case 7:
+          dialogOption = {
+            modelValue: user.cedula,
+            placeholder: 'Cambia la cedula del usuario.',
+            arialLabel: "editDialog",
+            save: function (input) {
+              $log.log("Updating.....");
+              user.cedula = input.$modelValue;
+              $log.log("Done.");
+            },
+            targetEvent: event,
+            validators: {
+              'md-maxlength': 40
+            }
+          };
+          break;
+      }
+      return dialogOption;
+    }
   }
 })();
