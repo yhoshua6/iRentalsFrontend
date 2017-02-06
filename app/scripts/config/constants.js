55a363382a28afd93effe521c924a2369c33b8a1
José <jose.epifanio90@gmail.com>
2017-02-05 22:04:07 -0600

    changed the endpoint.

--- app/scripts/config/constants.js
+++ app/scripts/config/constants.js
@@ -4,12 +4,12 @@
 (function () {
   "use strict";
   angular.module("iRentalsApp")
-    .constant("LOGIN_ENDPOINT", "http://localhost:3000/login")
-    .constant("PROPERTIES_ENDPOINT", "http://localhost:3000/properties")
-    .constant("INFO_USER_ENDPOINT", "http://localhost:3000/api/v1/info_users")
-    .constant("USER_ROLE_ENDPOINT", "http://localhost:3000/api/v1/user_roles")
-    .constant("USER_NOTIFICATIONS", "http://localhost:3000/api/v1/notifications")
-    .constant("USER_ALERTS", "http://localhost:3000/api/v1/alerts")
-    .constant("GROUP_ADMINS", "http://localhost:3000/api/v1/groups_admins")
-    .constant("GROUPS", "http://localhost:3000/api/v1/groups");
+    .constant("LOGIN_ENDPOINT", "https://irentalsapi.herokuapp.com/login")
+    .constant("PROPERTIES_ENDPOINT", "https://irentalsapi.herokuapp.com/properties")
+    .constant("INFO_USER_ENDPOINT", "https://irentalsapi.herokuapp.com/api/v1/info_users")
+    .constant("USER_ROLE_ENDPOINT", "https://irentalsapi.herokuapp.com/api/v1/user_roles")
+    .constant("USER_NOTIFICATIONS", "https://irentalsapi.herokuapp.com/api/v1/notifications")
+    .constant("USER_ALERTS", "https://irentalsapi.herokuapp.com/api/v1/alerts")
+    .constant("GROUP_ADMINS", "https://irentalsapi.herokuapp.com/api/v1/groups_admins")
+    .constant("GROUPS", "https://irentalsapi.herokuapp.com/api/v1/groups");
 })();
