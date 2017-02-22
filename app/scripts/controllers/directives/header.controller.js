769ab24189188efef5e60e014e4749c11a8e26ed
José <jose.epifanio90@gmail.com>
2017-02-22 11:13:31 -0600

    removed some useless files.2

---
+++ app/scripts/controllers/directives/header.controller.js
@@ -0,0 +1,36 @@
+/**
+ * Created by jose- on 12/02/2017.
+ */
+(function () {
+  "use strict";
+
+  angular.module("iRentalsApp")
+    .controller("headerCtrl", headerCtrl);
+
+  headerCtrl.$inject = ["$mdSidenav", "userInfoService"];
+  function headerCtrl($mdSidenav, userInfoService) {
+    var headerScope = this;
+    headerScope.userInfo = [
+      { title: "Nombre: ", value: userInfoService.user.name, enabled: true, type: "text"},
+      { title: "Teléfono: ", value: userInfoService.user.cellPhone, enabled: true, type: "numeric"},
+      { title: "Nombre del banco: ", value: userInfoService.user.bankName, enabled: true, type: "text"},
+      { title: "Clabe de Banco: ", value: userInfoService.user.bankClabe, enabled: true, type: "numeric"},
+      { title: "Número de Cuenta: ", value: userInfoService.user.bankAccount, enabled: true, type: "text"},
+      { title: "Cedula: ", value: userInfoService.user.cedula, enabled: true, type: "text"}
+    ];
+
+    headerScope.links = [
+      { linkTo: "root.properties", label: "Propiedades"},
+      { linkTo: "root.users", label: "Usuarios" },
+      { linkTo: "root.branches", label: "Ramas" },
+      { linkTo: "root.notifications", label: "Notificaciones" }
+    ];
+
+    headerScope.toggleLeft = buildToggler("userProfile");
+    //dashHeaderScope.toggleRight = buildToggler("userNotifications");
+
+    function buildToggler(componentId) {
+      return function() { $mdSidenav(componentId).toggle(); }
+    }
+  }
+})();
