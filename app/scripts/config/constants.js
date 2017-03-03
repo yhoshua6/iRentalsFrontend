884670ab63211520e10120db3735f3112a1793c4
Maria Fernanda Epifanio gamez <Marifer@Marias-MacBook-Air.local>
2017-03-02 23:18:58 -0600

    Changed api url.

--- app/scripts/config/constants.js
+++ app/scripts/config/constants.js
@@ -4,15 +4,15 @@
 (function () {
   "use strict";
   angular.module("iRentalsApp")
-    .constant("LOGIN_ENDPOINT", "http://localhost:3000/login")
-    .constant("PROPERTIES", "http://localhost:3000/api/v1/properties")
-    .constant("INFO_USER", "http://localhost:3000/api/v1/info_users")
-    .constant("USER_ROLES", "http://localhost:3000/api/v1/users_roles")
-    .constant("NOTIFICATIONS", "http://localhost:3000/api/v1/notifications")
-    .constant("USER", "http://localhost:3000/api/v1/users")
-    .constant("INFO_PROPERTIES", "http://localhost:3000/api/v1/info_properties")
-    .constant("PROPERTY_TYPES", "http://localhost:3000/api/v1/property_types")
-    .constant("BRANCHES", "http://localhost:3000/api/v1/branches")
-    .constant("BRANCHES_ROLES", "http://localhost:3000/api/v1/branch_roles")
-    .constant("PROPERTIES", "http://localhost:3000/api/v1/properties");
+    .constant("LOGIN_ENDPOINT", "https://chromabackend.herokuapp.com/login")
+    .constant("PROPERTIES", "https://chromabackend.herokuapp.com/api/v1/properties")
+    .constant("INFO_USER", "https://chromabackend.herokuapp.com/api/v1/info_users")
+    .constant("USER_ROLES", "https://chromabackend.herokuapp.com/api/v1/users_roles")
+    .constant("NOTIFICATIONS", "https://chromabackend.herokuapp.com/api/v1/notifications")
+    .constant("USER", "https://chromabackend.herokuapp.com/api/v1/users")
+    .constant("INFO_PROPERTIES", "https://chromabackend.herokuapp.com/api/v1/info_properties")
+    .constant("PROPERTY_TYPES", "https://chromabackend.herokuapp.com/api/v1/property_types")
+    .constant("BRANCHES", "https://chromabackend.herokuapp.com/api/v1/branches")
+    .constant("BRANCHES_ROLES", "https://chromabackend.herokuapp.com/api/v1/branch_roles")
+    .constant("PROPERTIES", "https://chromabackend.herokuapp.com/api/v1/properties");
 })();
