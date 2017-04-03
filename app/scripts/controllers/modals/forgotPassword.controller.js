d1e36392cdfd6aea5f7cbfc16968f09ddb63768c
Jose Epifanio <jose.epifanio90@gmail.com>
2017-04-02 21:45:33 -0500

    Send password to users.

--- app/scripts/controllers/modals/forgotPassword.controller.js
+++ app/scripts/controllers/modals/forgotPassword.controller.js
@@ -7,9 +7,20 @@
     .controller("forgotPwdCtrl", forgotPwdCtrl);
 
 
-  forgotPwdCtrl.$inject = ["$log", "$mdDialog"];
-  function forgotPwdCtrl($mdDialog) {
+  forgotPwdCtrl.$inject = ["$log", "$mdDialog", "requestService", "FORGOT_PWD"];
+  function forgotPwdCtrl($log, $mdDialog, requestService, FORGOT_PWD) {
     var forgotPwdScope = this;
+    forgotPwdScope.user = '';
+
+    forgotPwdScope.sendPwd = function () {
+      var data = {
+        user: forgotPwdScope.user
+      };
+      var pwdPromise = requestService.getPromise("POST", FORGOT_PWD, requestService.formatData(data), null);
+      pwdPromise.then(function (response) {
+        $log.log(response);
+      });
+    };
 
 
     forgotPwdScope.hide = function() {
